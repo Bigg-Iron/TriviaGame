f138b255b0ecd5c122b0b55a0493f417a29621a1
Bigg-Iron <lorenzor.bartolo@Lorenzos-MacBook-Pro.local>
2017-09-01 14:45:00 -0700

    Changed layout of html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
 
     console.log("hello console!");
-    
 
 
+    var clock = setInterval(myTimer, 1000);
 
+    function myTimer() {
+        var d = new Date();
+        document.getElementById("timer").innerHTML = d.toLocaleTimeString();
+    }
 
-
-
+   
 
 
 
